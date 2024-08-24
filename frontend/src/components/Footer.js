16185c3c8c230a50d859979816d78ebe7d228df5
obada kraishan <obada.kraishan@gmail.com>
2024-08-24 06:53:14 -0500

    Add Footer & Header

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Your Organization. All rights reserved.</p>
+      {/* Add links or additional info here */}
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Organizational Communication Network</h1>
+      {/* Add action buttons here if needed */}
+    </header>
+  );
+};
+
+export default Header;
